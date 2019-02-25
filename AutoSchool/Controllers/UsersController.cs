54121b6527b5948d1a6c6563365c414dbf1bf251
lalaka32 <lalaka32@mail.ru>
2019-02-25 19:31:52 +0300

    added post method

--- AutoSchool/Controllers/UsersController.cs
+++ AutoSchool/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Common.Entities;
 using DataService.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,6 +18,12 @@ namespace AutoSchool.Controllers
 		public UsersController(IUserStoreService userStoreService)
 		{
 			this.userStoreService = userStoreService;
+		}
+
+		public IActionResult Post(User user)
+		{
+			userStoreService.AddUser(user);
+			return Ok();
 		}
 	}
 }
