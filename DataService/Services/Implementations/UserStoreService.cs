c5df30246477b6e267e14a40043c489df426f759
lalaka32 <lalaka32@mail.ru>
2019-02-25 19:25:14 +0300

    Added user store service

---
+++ DataService/Services/Implementations/UserStoreService.cs
@@ -0,0 +1,24 @@
+﻿using Common.Entities;
+using DataService.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataService.Services.Implementations
+{
+	public class UserStoreService : IUserStoreService
+	{
+		readonly AutoSchoolContext storeContext;
+
+		public UserStoreService(AutoSchoolContext storeContext)
+		{
+			this.storeContext = storeContext;
+		}
+
+		public void AddUser(User user)
+		{
+			storeContext.Add(user);
+			storeContext.SaveChanges();
+		}
+	}
+}
