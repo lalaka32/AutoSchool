765b6fb6fbbadb796d6eb0cb179cab9201d78ff4
lalaka32 <lalaka32@mail.ru>
2019-02-25 18:48:04 +0300

    deleted cooments

--- AutoSchool/Startup.cs
+++ AutoSchool/Startup.cs
@@ -17,19 +17,16 @@ namespace AutoSchool
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-			// In production, the Angular files will be served from this directory
 			services.AddSpaStaticFiles(configuration =>
 			{
 				configuration.RootPath = "ClientApp/dist";
 			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -39,7 +36,6 @@ namespace AutoSchool
 			else
 			{
 				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 
@@ -56,8 +52,6 @@ namespace AutoSchool
 
 			app.UseSpa(spa =>
 			{
-				// To learn more about options for serving an Angular SPA from ASP.NET Core,
-				// see https://go.microsoft.com/fwlink/?linkid=864501
 
 				spa.Options.SourcePath = "ClientApp";
 
