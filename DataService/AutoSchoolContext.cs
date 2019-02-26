2e4b6dccd416cb14b88ce8e77b1239f8c29a2778
lalaka32 <lalaka32@mail.ru>
2019-02-26 09:27:09 +0300

    updated context

--- DataService/AutoSchoolContext.cs
+++ DataService/AutoSchoolContext.cs
@@ -8,27 +8,27 @@ namespace DataService
 {
 	public class AutoSchoolContext : DbContext
 	{
-		//public DbSet<AutoClassGroupe> AutoClassGroupes { get; set; }
+		public DbSet<User> Users { get; set; }
 
-		//public DbSet<AutoSchool> AutoSchools { get; set; }
-
-		//public DbSet<AutoSchoolAdmin> AutoSchoolAdmins { get; set; }
-
-		//public DbSet<CategoryOfDriverLicence> CategoryOfDriverLicences { get; set; }
-
-		//public DbSet<EmployeeLecturer> EmployeeLecturers { get; set; }
-
-		//public DbSet<Lecturer> Lecturers { get; set; }
-
-		//public DbSet<Qualifications> Qualifications { get; set; }
-
-		public DbSet<RulesSection> RulesSections { get; set; }
+		public DbSet<Role> Roles { get; set; }
 
 		public DbSet<TestResult> TestResults { get; set; }
 
 		public DbSet<TestRules> TestRules { get; set; }
 
-		public DbSet<User> Users { get; set; }
+		public DbSet<RulesSection> RulesSections { get; set; }
+		
+		public DbSet<AutoClassGroupe> AutoClassGroupes { get; set; }
+
+		public DbSet<Qualifications> Qualifications { get; set; }
+
+		public DbSet<CategoryOfDriverLicence> CategoryOfDriverLicences { get; set; }
+		
+		public DbSet<AutoSchoolAdmin> AutoSchoolAdmins { get; set; }
+
+		public DbSet<AutoSchool> AutoSchools { get; set; }
+
+		public DbSet<EmployeeLecturer> EmployeeLecturers { get; set; }
 
 		public AutoSchoolContext(DbContextOptions<AutoSchoolContext> options) : base(options)
 		{
