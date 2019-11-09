457a7b6003451f3b14ba5d18ffbcadd92b154053
Anton Marfutin <lalaka32@mail.ru>
2019-11-10 00:30:15 +0300

    automapper profiles auto registery (#15)
    * Added package for scanning assembly for mapping profiles in admin and api
    
    * Same for api, naming
    
    * Naming

---
+++ AutoSchool/Models/Mapper/DrivingTestWebMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoSchool.Models.Mapper
+{
+    public class DrivingTestWebMappingProfile : Profile
+    {
+    }
+}
